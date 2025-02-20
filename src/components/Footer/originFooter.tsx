68371b8b4bd5e07edd0024dc3e3c3a92ccbadf6c
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 02:02:21 +0900

    style: change footer

--- src/components/Footer/originFooter.tsx
+++ src/components/Footer/originFooter.tsx
@@ -3,12 +3,12 @@ import Contacts from './contacts';
 const OriginFooter = () => {
     return (
         <footer className="text-gray-200 bg-neutral-900">
-            <div className="max-w-6xl xl:max-w-6xl mx-auto px-8 py-10">
-                <div className="flex flex-col justify-between lg:flex-row">
-                    <p className="text-sm text-gray-200 font-medium mb-3 lg:mb-0">
-                        Ludens (루덴스, 경희대학교 소프트웨어융합대학 소속 게임 제작 동아리)
-                        <br/>
-                        ⓒ 2025 Ludens. All rights reserved.
+            <div className="max-w-7xl xl:max-w-7xl mx-auto px-8 py-10">
+                <div className="flex flex-col justify-between items-center lg:flex-row">
+                    <p className="text-md text-gray-200 font-medium mb-3 lg:mb-0">
+                        Copyright ⓒ 2025 Ludens. All rights reserved.
+                        <br />
+                        Created by <span className="text-white font-bold"><a href="https://grden.github.io/about/">김정원</a>, 최현승, 한채연</span>
                     </p>
                     <div className="flex flex-col lg:flex-row gap-2 items-start">
                         <span className="text-lg font-bold">Ludens와 소통하기</span>
