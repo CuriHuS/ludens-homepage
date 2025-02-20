812e6ea697bf240b060f28d6eca2e81f89d267a5
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 02:04:00 +0900

    style: rename header item

--- src/components/Header/menuItemList.ts
+++ src/components/Header/menuItemList.ts
@@ -1,6 +1,6 @@
 export const menuItemList = [
     { href: "/about", label: "소개" },
-    { href: "/project", label: "프로젝트" },
+    { href: "/project", label: "포트폴리오" },
     { href: "/activity", label: "활동" },
     { href: "/sponsor", label: "후원" },
 ];
