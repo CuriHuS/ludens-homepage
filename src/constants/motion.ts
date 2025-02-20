a40a8c56a3c05796ac0095aac6681aa038c7936d
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 02:07:37 +0900

    feat: add framer motion

---
+++ src/constants/motion.ts
@@ -0,0 +1,36 @@
+import { Variants } from 'framer-motion';
+
+export const defaultEasing = [0.6, -0.05, 0.01, 0.99];
+
+export const defaultFadeInVariants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 40,
+    transition: { duration: 0.5, ease: defaultEasing },
+    willChange: 'opacity, transform',
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, ease: defaultEasing },
+    willChange: 'opacity, transform',
+  },
+  exit: {
+    opacity: 0,
+    y: 40,
+    transition: { duration: 0.5, ease: defaultEasing },
+    willChange: 'opacity, transform',
+  },
+};
+
+export const staggerHalf: Variants = {
+  initial: { opacity: 0, y: 20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.05
+    }
+  }
+};
