6363af67ced2be0658904c30a99e1388acff87c0
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 23:35:49 +0900

    design: update margin

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     const router = useRouter();
     return (
         <div className="text-black bg-black">
-            <div className="flex flex-col justify-center mx-auto mt-52 text-center max-w-2xl">
+            <div className="flex flex-col justify-center mx-auto mt-64 text-center max-w-2xl">
                 <p className="text-9xl font-bold mb-2 tracking-tight text-white md:text-9xl">404</p>
                 <p className="text-2xl font-bold mb-6 tracking-tight text-gray-200 md:text-2xl">원하시는 페이지를 찾을 수 없습니다</p>
                 <button 
@@ -16,7 +16,7 @@ export default function NotFound() {
                     홈으로 가기
                 </button>
             </div>
-            <div className="mt-64"></div>
+            <div className="mt-52"></div>
         </div>
     )
 }
