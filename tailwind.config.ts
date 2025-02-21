73b5cc0d0b0676e1b3a484dc780ad3accb65ab7e
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 15:24:26 +0900

    chore: add tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'kyunghee-red': '#A40F16',
+        'kyunghee-red-20': '#F7A8AC',
+        'kyunghee-red-50': '#F1676E',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
       },
     },
   },
