c182a825bafef949c4ef896fc5cbe002d250d6ff
Jeongwon Kim <mickykim4@gmail.com>
2025-02-21 20:58:29 +0900

    style: remove unused code

--- src/components/common/ApplyButton/index.tsx
+++ src/components/common/ApplyButton/index.tsx
@@ -6,8 +6,7 @@ export default function ApplyButton() {
             href={APPLY_FORM_URL}
             target="_blank"
             rel="noopener noreferrer"
-            // className="px-6 py-1.5 text-lg font-extrabold text-white rounded-3xl bg-kyunghee-red hover:bg-gradient-to-r hover:from-kyunghee-red hover:to-white/10 hover:to-70% transition-all duration-1000 w-fit"
-            className="px-6 py-1.5 text-lg font-extrabold text-white rounded-3xl bg-gradient-to-r from-kyunghee-red via-kyunghee-red-50 via-50% to-kyunghee-red-20 to-80% bg-size-200 bg-pos-0 hover:bg-pos-100 transition-all duration-1000 w-fit"
+            className="px-6 py-1.5 text-lg font-extrabold text-white rounded-3xl bg-gradient-to-r from-kyunghee-red-950 via-kyunghee-red-500 via-50% to-kyunghee-red-300 to-80% bg-size-200 bg-pos-0 hover:bg-pos-100 transition-all duration-1000 w-fit"
         >
             지원하기
         </a>
