0cd8a2aad4c3b62d0bebda00a091eb347463fd43
Jeongwon Kim <mickykim4@gmail.com>
2025-02-26 18:26:33 +0900

    refactor: extract sponsor section

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -1,34 +1,18 @@
 "use client";
 
-import { LUDENS_EMAIL } from "@/constants/ludens";
-import { SPONSOR_LIST } from "@/constants/sponsor";
-import SponsorCard from "@/features/Sponsor/components/sponsorCard";
-import BannerSection from "@/components/common/banner";
-import SponsorBanner from "@/assets/banners/banner4.png";
+import BannerSection from "@/components/common/Banner";
+import sponsorBanner from "@/assets/banners/sponsor-banner.png";
+import SponsorSection from "@/features/Sponsor/sections/sponsorSection";
 
 export default function Sponsor() {
   return (
-    <section className="w-full flex flex-col mt-20 items-center">
-      <div className="relative w-screen min-h-[500px]">
-        <BannerSection
-          imageSrc={SponsorBanner}
-          title="Sponsors"
-          subtitle="를 도와주시는 기업들"
-        />
-        <div className="absolute inset-0 z-10 bg-gradient-to-b from-black via-transparent to-black h-full" />
-      </div>
-
-      <div className="w-fit grid grid-cols-1 gap-x-12 gap-y-0 md:grid-cols-2">
-        {SPONSOR_LIST.map((sponsor) => (
-          <SponsorCard key={sponsor.title} {...sponsor} />
-        ))}
-      </div>
-      <a
-        href={LUDENS_EMAIL}
-        className="mt-28 py-2.5 px-6 text-base font-semibold rounded-full bg-white text-black"
-      >
-        후원 문의
-      </a>
-    </section>
+    <main className="w-full flex flex-col items-center">
+      <BannerSection
+        imageSrc={sponsorBanner}
+        title="후원"
+        subtitle="루덴스를 도와주시는 기업들"
+      />
+      <SponsorSection />
+    </main>
   );
 }
---
+++ src/features/Sponsor/sections/sponsorSection.tsx
@@ -0,0 +1,16 @@
+import { LUDENS_EMAIL } from '@/constants/ludens';
+import { SPONSOR_LIST } from '@/constants/sponsor';
+import SponsorCard from '../components/sponsorCard';
+
+export default function SponsorSection() {
+    return (
+        <section className="w-full flex flex-col items-center">
+            <div className="w-fit grid grid-cols-1 gap-x-12 gap-y-0 md:grid-cols-2">
+                {SPONSOR_LIST.map((sponsor) => (
+                    <SponsorCard key={sponsor.title} {...sponsor}/>
+                ))}
+            </div>
+            <a href={LUDENS_EMAIL} className="mt-28 py-2.5 px-6 text-base font-semibold rounded-full bg-white text-black">후원 문의</a>
+        </section>
+    )
+}
