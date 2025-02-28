15b037794b9b04284c8ee5f55998ad4ff17114a1
Jeongwon Kim <mickykim4@gmail.com>
2025-02-28 11:56:48 +0900

    style: change url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,25 +7,25 @@ import { MetadataRoute } from 'next';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url: 'https://ludens.kr/about',
+            url: 'https://ludens-homepage.vercel.app/about',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1.0,
         },
         {
-            url: 'https://ludens.kr/activity',
+            url: 'https://ludens-homepage.vercel.app/activity',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://ludens.kr/project',
+            url: 'https://ludens-homepage.vercel.app/project',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://ludens.kr/sponsor',
+            url: 'https://ludens-homepage.vercel.app/sponsor',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.6,
