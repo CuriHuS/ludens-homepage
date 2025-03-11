ccfc40bc8394e841d9a18fbf4e33aa0852317337
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-03-12 00:05:46 +0900

    design:UI fix

--- src/features/About/components/countupCard.tsx
+++ src/features/About/components/countupCard.tsx
@@ -1,32 +1,39 @@
 import CountUp from "react-countup";
 
 export type CountupCardProps = {
-    title: string;
-    number: number;
-    unit: string;
-}
+  title: string;
+  number: number;
+  unit: string;
+};
 
 export default function CountupCard({ title, number, unit }: CountupCardProps) {
-    return (
-        <CountUp
-            start={0}
-            end={number}
-            duration={3}
-            enableScrollSpy={true}
-            scrollSpyOnce={true}
-            separator=""
-        >
-            {({ countUpRef }) => (
-                <div className="flex flex-col items-center justify-center bg-neutral-800 rounded-xl p-8 w-full aspect-[2/1]">
-                    <p className="text-sm md:text-lg text-white font-medium mb-1">{title}</p>
-                    <p className="flex items-center">
-                        <span className="text-2xl md:text-4xl text-white font-extrabold " ref={countUpRef}>
-                            {number}
-                        </span>
-                        <span className="text-2xl md:text-4xl text-white font-extrabold">{unit}</span>
-                    </p>
-                </div>
-            )}
-        </CountUp>
-    );
+  return (
+    <CountUp
+      start={0}
+      end={number}
+      duration={3}
+      enableScrollSpy={true}
+      scrollSpyOnce={true}
+      separator=""
+    >
+      {({ countUpRef }) => (
+        <div className="flex flex-col items-center justify-center bg-neutral-800 rounded-xl p-8 w-full aspect-[2/1]">
+          <p className="text-[10px] md:text-lg text-white font-medium mb-1">
+            {title}
+          </p>
+          <p className="flex items-center">
+            <span
+              className="text-2xl md:text-4xl text-white font-extrabold "
+              ref={countUpRef}
+            >
+              {number}
+            </span>
+            <span className="text-2xl md:text-4xl text-white font-extrabold">
+              {unit}
+            </span>
+          </p>
+        </div>
+      )}
+    </CountUp>
+  );
 }
