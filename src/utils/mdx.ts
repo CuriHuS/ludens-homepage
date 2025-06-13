e796fe0866a5389060184e3463ddef52cb97aa1e
Jeongwon Kim <mickykim4@gmail.com>
2025-06-13 19:32:40 +0900

    fix: fix eslint error

--- src/utils/mdx.ts
+++ src/utils/mdx.ts
@@ -5,21 +5,6 @@ import { promisify } from 'util';
 
 // Convert fs methods to promises
 const readFile = promisify(fs.readFile);
-const readdir = promisify(fs.readdir);
-
-// Define types for MDX modules
-type MDXContent = React.ComponentType<any>;
-
-interface MDXModule {
-    default: MDXContent;
-    frontmatter?: Record<string, any>;
-}
-
-// Helper type for the return value of getProjectContent
-interface ProjectContent {
-    Component: MDXContent;
-    frontmatter: Record<string, any>;
-}
 
 /**
  * Get MDX content for a specific project by id
@@ -36,35 +21,7 @@ export async function getProjectContent(id: string) {
             content,
             frontmatter
         };
-    } catch (error) {
+    } catch {
         return null;
     }
 }
-
-// export async function getProjectList() {
-//     const projectDir = path.join(process.cwd(), 'src/content/projects');
-    
-//     try {
-//         const files = await readdir(projectDir);
-        
-//         const projects = await Promise.all(
-//             files
-//                 .filter(file => file.endsWith('.mdx'))
-//                 .map(async (file) => {
-//                     const filePath = path.join(projectDir, file);
-//                     const fileContent = await readFile(filePath, 'utf-8');
-//                     const { data: frontmatter } = matter(fileContent);
-                    
-//                     return {
-//                         ...frontmatter,
-//                         id: file.replace('.mdx', ''),
-//                     };
-//                 })
-//         );
-        
-//         return projects;
-//     } catch (error) {
-//         console.error('Error reading projects:', error);
-//         return [];
-//     }
-// }
